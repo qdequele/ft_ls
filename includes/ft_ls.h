b729deb872bece04d2ac87ea8286b0e77b18a339
qdequele <quentin@dequelen.me>
2016-03-02 15:41:15 +0100

    struct for define params

---
+++ includes/ft_ls.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qdequele <qdequele@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/02 15:21:17 by qdequele          #+#    #+#             */
+/*   Updated: 2016/03/02 15:33:52 by qdequele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+# include <libft.h>
+
+typedef struct      s_opt
+{
+    int             l;
+    int             R;
+    int             a;
+    int             r;
+    int             t;
+}                   t_opt;
+
+typedef struct      s_env
+{
+
+}                   t_env;
+
+#endif
